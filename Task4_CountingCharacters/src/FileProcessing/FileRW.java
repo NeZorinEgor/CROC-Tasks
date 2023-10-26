374d7a417ffe7eb49992e165364cda96e788f442
Zorin Egor <zorin-ep@mail.ru>
2023-10-26 22:42:30 +0800

    add main logic

--- Task4_CountingCharacters/src/FileProcessing/FileRW.java
+++ Task4_CountingCharacters/src/FileProcessing/FileRW.java
@@ -1,20 +1,18 @@
 package FileProcessing;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class FileRW {
-    public static void counter() throws FileNotFoundException {
+    public static void counter() throws IOException {
         File inputFile = new File("src/file/input.txt");
         Scanner in = new Scanner(inputFile);
         int characterCount = 0;
-
+        FileWriter fw = new FileWriter("src/file/output.txt");
         while (in.hasNext()) {
             String line = in.nextLine();
             characterCount += line.length();
         }
-
         System.out.println("Количество символов в файле: " + characterCount);
         in.close();
     }
--- Task4_CountingCharacters/src/Main.java
+++ Task4_CountingCharacters/src/Main.java
@@ -1,10 +1,9 @@
-
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import FileProcessing.FileRW;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         FileRW.counter();
     }
 }
